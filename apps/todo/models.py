d1a2b5e5a24045d548de0eb03643e6e5a139a267
Antonio Yves <yvissousa@gmail.com>
2024-11-29 13:00:00 -0300

    feat: add model todo

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    done_at = models.DateTimeField(null=True)
+
+    class Meta:
+        verbose_name = 'ToDo'
+        verbose_name_plural = 'ToDos'
+
+    def __str__(self):
+        return f'Title: {self.title} -> Created at: {self.created_at}'
+
+    def __repr__(self):
+        return self.__str__()
